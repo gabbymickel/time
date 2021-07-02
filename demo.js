ac3c12f127ee406b6b03abb6f1eeb77a71704749
Gabriella Mickel <gabbymickel@gmail.com>
2021-07-01 21:27:02 -0400

    first commit

---
+++ demo.js
@@ -0,0 +1,151 @@
+const DATA = [
+  {
+    year: '1880',
+    title: 'Draining the Great Black Swamp',
+    body: '<br>With the invention of the Buckeye Traction Ditcher, settlers drained the Great Black Swamp, which was 2x larger than the Everglades. The Great Black Swamp stretched westward from the Lake Erie into eastern Indiana. This period also marked the start of Ohioans dumping human, agricultural, and industrial waste into the lake. Unbeknownst to the settlers, the swamp filtered the lake’s water acting as Lake Erie’s kidney. Draining this wetland has left Lake Erie susceptible to Harmful Algal Blooms.',
+    image: '/images/blackSwamp.png'
+  },
+  {
+    year: '1933',
+    title: 'The First Blooms',
+    body: '<br> In the summer of 1933, western Lake Erie "looked as if it were coated with green paint" - Dr. Clarence Taft of Ohio State University. Ohioans began complaining about the foul smell. By the 1940\'s and 50\'s, many beaches were closed as health hazards. Due to waste disposal in Lake Erie by cities like Toledo and Clevland, the Lake\'s shore wateres were becoming the functional equivalent of a septic tank.',
+    image: '/images/taft.png'
+  },
+  {
+    year: '1953',
+    title: 'Scientific Signs of Trouble',
+    body: '<br> In the summer of 1953, scientists began to realize that the trophic state of the Lake was shifting. Dr. Alfred M. Beeton found a 2,600 square mile area at the bottom of the Lake that had no oxygen at all. However, no progress was made to stop the pollution driving these changes because most people at that time thought the Lakes were too big to pollute and anything put into the Lakes would be sufficiently diluted. <br><br> Less oxygen dissolved in the water is often referred to as a “dead zone” because most aquatic life either dies, or if they are mobile like fish, they can leave the area. The red zones are dead zones.',
+    image: '/images/deadzone.jpeg'
+  },
+  {
+    year: '1960',
+    title: 'Infrastructure Challenges Develop',
+    body: '<br> Since the late 1960s (High, 2003), the Great Lakes region has experienced a steady decline in industry and population. Many cities are now struggling with shrinking tax bases and increasing demands on infrastructure investment as systems age and become outdated. Climate change has only amplified these infrastructure challenges by increasing the amount of precipitation falling in the heaviest storms in the region by 37% since the 1950’s (United States Global Change Research Program, 2014).',
+    image: '/images/rain.jpeg'
+  },
+  {
+    year: '1960',
+    title: 'Lake Erie Declared Dead',
+    body: '<br> Reporters dramatically declared Lake Erie’s death in the 1960s because the lake experienced cultural eutrophication. Eutrophication occurs when excessive richness of nutrients in a lake causes a dense growth of plant life, typically algae, and death of animal life from lack of oxygen. Cultural eutrophication refers to the same process sped up by human activity, which could include dumping excess nutrients from sewage, detergents, fertilizers, etc. into the lake. The algae from eutrophication can turn a once clear lake into what looks like a lake full of green paint, which is what happened to Lake Erie in the 1960s as schools of suffocated fish full of toxic pollutants washed ashore.',
+    image: '/images/death.png'
+  },
+  {
+    year: '1965',
+    title: 'Grassroots Action',
+    body: '<br> In 1965, David Blaushild, a car dealer from Clevland, collected over a million signatures on a petition to "Save Lake Erie". Ohio\'s Governor, James A. Rhodes, used this petition to push the federal government to take action. This resulted in multiple studies, one by the IJC. <br><br> As a result of a single citizen\'s petition, the U.S. Public Health Setvice held HEATED hearings on the problems of Lake Erie in 1965. States pointed fingers at each other, while industries and cities pushed back against pending regulation with the sponsorship of the U.S. Chamber of Commerce. ',
+    image: '/images/underwear.jpeg'
+  },
+  {
+    year: '1965',
+    title: 'Enforcement Confrences',
+    body: '<br> Federal officials of the new Federal Water Pollution Control Administration began a round of "enforcement confrences" in the Great Lakes region to promote upgrades to waste treatment requirements. This marks the shift from the "reasonable use" doctrine that gave dischargers a legitimate right to use a body of water for waste disposal, as long as its primary uses were protected. The goal of the officials was to encourage maximum feasible waste treatment. At the Lake Erie conference, David Blaushild described his community\'s impatience with the regulatory system. ',
+    image: ''
+  },
+  {
+    year: '1970',
+    title: 'Lake Erie Becomes a Symbol for the Environmental Movement',
+    body: '<br> Lake Erie, the dead lake, and its tributary the Cuyahoga River, the burning river, became symbols for the larger environmental movement happening in the United States that led to the establishment of the Environmental Protection Agency and the Clean Water Act.',
+    image: '/images/savelake.png'
+  },
+  {
+    year: '1971',
+    title: 'The Struggle with Pollution Continues',
+    body: '<br> By 1971, the collective mindset shifted from the Lakes being too big to pollute to taking responsibility for the actions causing the degredation of Lake Erie. In 1971, Walt Kelly rephrased Commodore Perry\'s (who earned the title "Hero of Lake Erie" for leading American forces in a decisive naval victory at the Battle of Lake Erie during the War of 1812) iconic line "We have met the enemy and he is ours." to "We have met the enemy and he is us." to comment on the state of Lake Erie for his Earth Day piece. A great deal of legislation from the municipal to federal level followed. <a href="https://library.osu.edu/site/40stories/2020/01/05/we-have-met-the-enemy/">Billy Ireland Cartoon Library at Ohio State</a>',
+    image: '/images/earthdaypogo.png'
+  },
+
+  {
+    year: '1972',
+    title: 'The Great Lakes Water Quality Agreement',
+    body: '<br> The Great Lakes Water Quality Agreement, first signed by Canada and the United States in 1972 and updated in 1978, 1987 and 2012, commits both countries to “restore and maintain the chemical, physical, and biological integrity of the Waters of the Great Lakes." The Maumee River is currently designated as an area of concern, as it is one of the most polluted areas.',
+    image: '/images/AOC.png'
+  },
+
+  {
+    year: '1972',
+    title: 'The US Clean Water Act',
+    body: '<br> A set of massive amendments to the Federal Water Pollution Control Act of 1956, the CWA set water-quality guidelines and industrial- and sewage-effluent standards, provided for penalties against polluting industries, and commited the federal government to supply funding for building and upgrading sewage-treatment plants. <br><br> Cities on Lake Erie, like Detroit, Toledo, and Clevland could not longer skate by with their primary-treatment-only facilities. That alone made a huge difference to Lake Erie. Unfortunately, the legislators failed to address the detergent industry or agriculture directly.',
+    image: '/images/nixon.png'
+  },
+
+  {
+    year: '1972',
+    title: 'Local Laws Tackle the Detergent Industry',
+    body: 'Left by the federal government to fight to protect their fresh water sources on their own, cities passed local phosphate-detergent bans. While some industries self-regulated (like the steel industry) after learning about the environmental impacts of their actions, the detergent industry fought Akron all the way from city council chambers to federal court. After winning a temporary injuction, a federal judge through the Detergent Industry\'s case out as frivolous and with that precedent cities were able to defend their phosphate-detergent bans. ',
+    image: '/images/akron.png'
+  },
+
+  {
+    year: '1975',
+    title: 'Signs of Improvement',
+    body: 'In 1975, the summer algal bloom in Lake Erie failed to appear for the first time in 10 years. The beaches remained open, the anoxcic desert did not develop, and the Lake itself turned from green back to blue. Phosphate loading had gone down by 50%. Many thought the Lake had been saved from the brink of death. ',
+    image: ''
+  },
+
+  {
+    year: '1985',
+    title: 'Dr. Seuss Removes his Lake Erie Dis',
+    body: ' In 1971, the pollution of Lake Erie warranted a direct mention by Dr. Seuss in The Lorax, a book chronicling the dangers of mismanaging the environment. Fourteen years after Dr. Seuss wrote The Lorax, he removed the direct mention of Lake Erie after hearing about the clean-up of the lake. Thanks to the aforementioned improvements and the lake’s short retention time, conditions improved enough for most concerned citizens to stop paying attention. ',
+    image: '/images/seuss.png'
+  },
+
+  {
+    year: '1990',
+    title: 'The Blooms Return',
+    body: ' By the mid-1990s, due to the failure to address agricultural nutrient pollution, the algal blooms returned and the dead zones came with them.  ',
+    image: ''
+  },
+
+  {
+    year: '1990',
+    title: 'Invasive Zebra and Quagga Mussels',
+    body: ' In the 1990s zebra and quagga mussels, an invasive species, began to populate Lake Erie. These mussels know what microcystis taste like and they don\'t consume it, so they\'re decimating algae species while leaving the toxic algae. <b> This means the HABs of today are more dangerous than the HABs from the 1900s. ',
+    image: '/images/mus.jpeg'
+  },
+
+  {
+    year: '2013',
+    title: 'Toledo Stormwater Credit Program',
+    body: '<br> In Toledo, to help offset the cost of stormwater management, property owners are subject to a utility charge that varies based on the amount of hard surface on their property. They can reduce their fee by implementing certain practices that reduce the impact of stormwater from their property. Stormwater credits are available to non-residential property owners who prove to be good stewards of stormwater resources.',
+    image: '/images/stormwater.png'
+  },
+
+  {
+    year: '2014',
+    title: 'Toledo Water Crisis',
+    body: 'On August 2nd, 2014, residents of Toledo, Ohio awoke to urgent warnings not to drink or use their tap water. Half a million people were unable to drink their water, cook with it, or brush their teeth. Boiling it, would have made it even more toxic. The grocery store shelves were wiped clean of bottled water almost instantly. People waited in long lines behind trucks full of water brought in by the National Guard. How did this happen? The summer algal bloom floated into the water treatment plant and overwhelmed the system. This crisis spurred grassroots action and legislation in Ohio.',
+    image: '/images/toledo.jpg'
+  },
+
+  {
+    year: '2019',
+    title: 'Lake Erie Bill of Rights',
+    body: '<br> A citizen initiative, supported by the Community Environmental Legal Defense Fund and Toledoans for Safe Water, to include a law in the city charter holding liable any public or private entity that violates what proponents termed the "Lake Erie Bill of Rights" was on the ballot for Toledo voters in Lucas County, Ohio, on February 26, 2019. Although a majority of voters approved the initiative, it was overturned in the courts. On February 27, 2020, U.S. District Court Judge Jack Zouhary ruled that LEBOR was invalid in its entirety because it was “unconstitutionally vague and exceeds the power of municipal government in Ohio.” Toledoans for Safe Water, who advocated for the initiative, said in response to the ruling, "In an unfortunate but predictable decision, he invalidated the democratically enacted Lake Erie Bill of Rights. This local charter amendment was the first of its kind in the United States."',
+    image: '/images/LEBOR.png'
+  },
+  {
+    year: '2019',
+    title: 'State Legislation to Preempting Rights of Nature',
+    body: '<br> On July 18, 2019, Gov. Mike DeWine (R) signed legislation that was included in the state budget bill that prohibits legal actions on behalf of nature or an ecosystem. The provision stated, "Nature or any ecosystem does not have standing to participate in or bring an action in any court of common pleas. No person, on behalf of or representing nature or an ecosystem, shall bring an action in any court of common pleas.”',
+    image: '/images/dewinebarf.png'
+  },
+  
+  {
+    year: '2019',
+    title: 'H2Ohio',
+    body: 'In 2019, Ohio Governor Mike DeWine introduced the H2Ohio plan. Backed by $172 million in funding, the ODA, and the ODNR, the plan includes wetland creation and restoration (to do the job the Great Black Swamp once did) & the implementation of agricultural best practices.',
+    image: '/images/h2ohiop.jpeg'
+  },
+  {
+    year: '2020',
+    title: 'ELPC and Citizen Suits Rejected',
+    body: 'As the HABs persist in Lake Erie, Toledoans have fought tooth and nail for help. The courts have refused to help and the regulatory agencies continue to drag their feet. In a 2020 opinion (plaintiffs the Environmental Law and Policy Center, Michael Ferner, and Susan Matz filed their first suit against the U.S. EPA, its Administrator Scott Pruitt, and Acting Regional Administrator, Scott Kaplan), Judge Carr cited "Ohio’s persistent failures" and "Ohio’s Noncompliance With the CWA" did nothing but remand the case to the U.S. EPA. In 2021, Ohio\'s state court system rejected arguments based on the public trust doctine and the Ohio Constitution. ',
+    image: '/images/courtopinion.png'
+  },
+ 
+];
+
+if(document.getElementById('timeline')) {
+  let timeline = new Timeline('timeline', DATA);
+  timeline.init();
+}
